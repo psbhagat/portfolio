42e67338f33028fd4728d277a8142f6fde930a9b
[Priyanka Bhagat] <[p.bhagat0312@gmail.com]>
2021-01-21 23:10:24 +0530

    changing image urls

--- src/app/core/providers/skills-mock.ts
+++ src/app/core/providers/skills-mock.ts
@@ -1,12 +1,12 @@
 import { Skill } from './skill';
 
 export const SKILLS: Skill[] = [
-    { id: 1, name: '../../../assets/images/html5.png', altText: 'HTML5' },
-    { id: 2, name: '../../../assets/images/css3.png', altText: 'CSS' },
-    { id: 3, name: '../../../assets/images/angular.png', altText: 'ANGULAR' },
-    { id: 4, name: '../../../assets/images/javascript.png', altText: 'JAVASCRIPT' },
-    { id: 5, name: '../../../assets/images/sass.png', altText: 'SASS' },
-    { id: 6, name: '../../../assets/images/bootstrap.png', altText: 'BOOTSTRAP' },
-    { id: 7, name: '../../../assets/images/github.png', altText: 'GITHUB' },
-    { id: 8, name: '../../../assets/images/spring.png', altText: 'SPRING' },
+    { id: 1, name: 'assets/images/html5.png', altText: 'HTML5' },
+    { id: 2, name: 'assets/images/css3.png', altText: 'CSS' },
+    { id: 3, name: 'assets/images/angular.png', altText: 'ANGULAR' },
+    { id: 4, name: 'assets/images/javascript.png', altText: 'JAVASCRIPT' },
+    { id: 5, name: 'assets/images/sass.png', altText: 'SASS' },
+    { id: 6, name: 'assets/images/bootstrap.png', altText: 'BOOTSTRAP' },
+    { id: 7, name: 'assets/images/github.png', altText: 'GITHUB' },
+    { id: 8, name: 'assets/images/spring.png', altText: 'SPRING' },
 ];
